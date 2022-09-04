79937f0ba45d36671a11161298610fddf2dc7b5c
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2022-09-04 21:21:29 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -25,8 +25,9 @@ from datetime import datetime
 
 def stats(update, context):
     if ospath.exists('.git'):
-        last_commit = check_output(["git log -1 --date=short --pretty=format:'%cd <b>From</b> %cr'"], shell=True).decode()
-         last_commit = 'No UPSTREAM_REPO'
+        last_commit = check_output(["git log -1 --date=short --pretty=format:'%cd <b>From</b> %cr'"], shell=True).decode()    
+    else:
+        last_commit = 'No UPSTREAM_REPO'
     currentTime = get_readable_time(time() - botStartTime)
     current = now.strftime('%m/%d %I:%M:%S %p')
     osUptime = get_readable_time(time() - boot_time())
